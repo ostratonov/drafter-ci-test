fdea6dede13c25fb7ab731b37f669c5dfdebed63
Oleksander Stratonov <sstratonov@me.com>
2023-04-19 15:42:21 +0300

    patch: [BLS-2028] - check pt3

--- .releaserc.js
+++ .releaserc.js
@@ -1,44 +1,36 @@
-'use strict';
+'use strict'
 
-const assert = require('assert')
 const env = process.env.NODE_ENV
 
-const configurations = {
-    dev: {
-        branches: ['dev'],
-        tagFormat: 'v${version}-dev',
-        plugins: [
-            "@semantic-release/commit-analyzer",
-            "@semantic-release/release-notes-generator",
-            "@semantic-release/github",
-            ["@semantic-release/npm", {
-                "npmPublish": false,
+const liveConfig = {
+    branches: ['main'],
+    tagFormat: 'v${version}-live',
+    plugins: [
+        "@semantic-release/commit-analyzer",
+        "@semantic-release/release-notes-generator",
+        "@semantic-release/github",
+    ]
+}
+
+const devConfig = {
+    branches: ['dev'],
+    tagFormat: 'v${version}-dev',
+    plugins: [
+        "@semantic-release/commit-analyzer",
+        "@semantic-release/release-notes-generator",
+        "@semantic-release/github",
+        ["@semantic-release/npm", {
+            "npmPublish": false,
+        }
+        ],
+        [
+            "@semantic-release/git",
+            {
+                "assets": ["package.json"],
+                "message": "chore(release): ${nextRelease.version} [ci skip]\n\n${nextRelease.notes}"
             }
-            ],
-            [
-                "@semantic-release/git",
-                {
-                    "assets": ["package.json"],
-                    "message": "chore(release): ${nextRelease.version} [ci skip]\n\n${nextRelease.notes}"
-                }
-            ]
         ]
-    },
-    live: {
-        branches: ['main'],
-        tagFormat: 'v${version}-live',
-        plugins: [
-            "@semantic-release/commit-analyzer",
-            "@semantic-release/release-notes-generator",
-            "@semantic-release/github",
-        ]
-    }
+    ]
 }
 
-module.exports = () => {
-    assert(false, 'NODE_ENV is not set. Check your environment .yml file.')
-
-    console.log(configurations[env])
-
-    return configurations[env]
-}
+module.exports = env === 'live' ? liveConfig : devConfig
