c5567ed4ff199a816bdd48c7e3baec132a954e3a
Oleksander Stratonov <sstratonov@me.com>
2023-04-24 11:00:19 +0300

    feat(clean): try different configurations

--- .releaserc.js
+++ .releaserc.js
@@ -2,37 +2,74 @@
 const env = process.env.NODE_ENV
 
 const liveConfig = {
-    branches: ['main'],
-    tagFormat: 'v${version}-live',
-    plugins: [
-        "@semantic-release/commit-analyzer",
+    "branches": [
+        "master"
+    ],
+    "tagFormat": "v${version}",
+    "plugins": [
+        [
+            "@semantic-release/commit-analyzer",
+            {
+                "preset": "angular",
+                "releaseRules": [
+                    {
+                        "type": "refactor",
+                        "scope": "core-*",
+                        "release": "minor"
+                    },
+                    {
+                        "type": "refactor",
+                        "release": "patch"
+                    },
+                    {
+                        "scope": "no-release",
+                        "release": false
+                    }
+                ],
+                "parserOpts": {
+                    "noteKeywords": [
+                        "BREAKING"
+                    ]
+                }
+            }
+        ],
         "@semantic-release/release-notes-generator",
         "@semantic-release/github",
-        ["@semantic-release/exec", {
-            "successCmd": "yarn jira:sync \"${nextRelease.notes}\" \"${nextRelease.version}\""
-        }]
-    ]
-}
-
-const devConfig = {
-    branches: ['dev'],
-    tagFormat: 'v${version}-dev',
-    plugins: [
-        "@semantic-release/commit-analyzer",
-        "@semantic-release/release-notes-generator",
-        "@semantic-release/github",
-        ["@semantic-release/npm", {
-            "npmPublish": false,
-        }
+        [
+            "@semantic-release/npm",
+            {
+                "npmPublish": false
+            }
         ],
         [
             "@semantic-release/git",
             {
-                "assets": ["package.json"],
-                "message": "chore(release): ${nextRelease.version} [ci skip]\n\n${nextRelease.notes}"
+                "assets": [
+                    "package.json"
+                ],
+                "message": "release: ${nextRelease.version} [ci skip]\n\n${nextRelease.notes}"
+            }
+        ],
+        [
+            "@semantic-release/exec",
+            {
+                "successCmd": "yarn jira:sync \"${nextRelease.notes}\" \"${nextRelease.version}\""
             }
         ]
+    ]
+}
 
+const devConfig = {
+    branches: ['dev', 'sandbox', 'rc'],
+    plugins: [
+        "@semantic-release/commit-analyzer",
+        "@semantic-release/release-notes-generator",
+        [
+            "@semantic-release/exec",
+            {
+                "successCmd": "yarn jira:sync \"${nextRelease.notes}\" \"${nextRelease.version}\""
+            }
+        ]
     ]
 }
 
