5b754a95825f13f28b78e539a7d091d926b2c21a
Oleksander Stratonov <sstratonov@me.com>
2023-04-24 11:09:48 +0300

    feat(clean): try branch

--- scripts/jira/index.js
+++ scripts/jira/index.js
@@ -5,7 +5,7 @@
 const processTasks = require('./process-tasks')
 
 processTasks().then(() => process.exit(0)).catch(e => {
-    console.log(Error(e).stack)
+    console.log(Error(e).stack, e)
 
     process.exit(-1)
 })
