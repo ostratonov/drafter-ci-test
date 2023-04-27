1cf8fb3930466f31b194d3d77bbf86666ff8651b
Oleksander Stratonov <sstratonov@me.com>
2023-04-27 12:27:29 +0300

    some false commit

--- scripts/jira/process-tasks.js
+++ scripts/jira/process-tasks.js
@@ -2,7 +2,7 @@
 
 module.exports = () => {
     console.log('Args', process.argv)
-    console.log('Checking if the script with current configuration')
+    console.log('Checking if the script with current configuration.')
 
     return Promise.resolve()
 }
