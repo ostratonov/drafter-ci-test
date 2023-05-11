fb34d690880d0aeb0564f32a988a7b814ed700d3
Oleksander Stratonov <sstratonov@me.com>
2023-05-11 12:19:07 +0300

    pushy pt.3

--- scripts/jira/process-tasks.js
+++ scripts/jira/process-tasks.js
@@ -2,6 +2,7 @@
 
 module.exports = () => {
     console.log('Args', process.argv)
+    console.log('Pr Name', process.argv[2])
     console.log('Checking if the script with current configuration.')
 
     return Promise.resolve()
