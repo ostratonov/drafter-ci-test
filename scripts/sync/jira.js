2eacfbc6f66e2550fcc679a5ab5289a5c1eb962a
Oleksander Stratonov <sstratonov@me.com>
2023-06-07 14:21:57 +0300

    schema(deployments): schwma (BLS-2)

--- scripts/sync/jira.js
+++ scripts/sync/jira.js
@@ -1,10 +1,17 @@
 "use strict";
 
+const getTicketAliases = patchNotes => {
+    const ticketRegex = /BLS-\d+/gi
+
+    const matches = patchNotes.match(ticketRegex) || []
+
+    return matches.map(match => match.toUpperCase())
+}
+
 module.exports = (patchNotes, patchVersion, options) => {
+    const aliases = getTicketAliases(patchNotes)
+    console.log('aliases', aliases)
     console.log('notes', patchNotes)
-    console.log('version', patchVersion)
-    console.log('args', options)
-    console.log('Checking if the script with current configuration.')
 
     return Promise.resolve()
 }
